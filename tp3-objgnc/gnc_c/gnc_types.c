622c0ec5865682ad18ffe82085a90fc74607fc9a
sami <sami@samis-mac-mini.home>
2024-10-17 20:44:27 +0200

    gitignore

--- tp3-objgnc/gnc_c/gnc_types.c
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 17/10/2024 at 12:52 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled fri. sep. 27 23:48:47 CET 2024) --- */
-/* --- Command line: /home/sami/.opam/default/bin/heptc -target c gnc.ept --- */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "gnc_types.h"
-
